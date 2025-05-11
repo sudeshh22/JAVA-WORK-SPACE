dbf52dd59dbb5b56e2899d3b46a002db71bbca8b
sudeshh22 <sudeshhy@gmail.com>
2025-05-11 13:46:02 +0530

    Create DivisibleBy7.java

---
+++ DivisibleBy7.java
@@ -0,0 +1,14 @@
+public class DivisionBy7 {
+    public static void main(String[]args){
+        int n=48;
+        int quotient=n/7;
+        int product=quotient*7;
+        if(n==product){
+            System.out.print("it is divisible");
+        }
+        else{
+            System.out.print("it is not divisible");
+        }
+    }
+    
+}
